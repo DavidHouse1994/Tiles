013ffe754a15af3eb91598eb85a6c4e5dda0865b
DavidHouse1994 <davidbrynnhouse@gmail.com>
2023-04-17 13:55:07 -0400

    implement jump

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption("Pygame window")
 
-WINDOW_SIZE = (400, 400)
+WINDOW_SIZE = (600, 400)
+
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+display = pygame.Surface((300, 200))
 
 pygame.init()
 
 player = pygame.image.load('player.png')
 grass = pygame.image.load('grass.png')
 dirt = pygame.image.load('dirt.png')
+TILE_SIZE = grass.get_width()
 
 game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
@@ -28,39 +33,79 @@ game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'
             ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
 
 
-player_location = [40, 40]
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+def move(rect, movement, tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
+
 moving_right = False
 moving_left = False
 
 player_y_momentum = 0
 
-player_rect = pygame.Rect(player_location[0], player_location[1], player.get_width(), player.get_height())
+player_rect = pygame.Rect(50, 50, player.get_width(), player.get_height())
 test_rect = pygame.Rect(100, 100, 100, 50)
 
-screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
 while True:
-    screen.fill((0, 0, 0))
-    screen.blit(player, player_location)
+    display.fill((135, 206, 235))
     
-    if player_location[1] > WINDOW_SIZE[1]-player.get_height():
-        player_y_momentum = -player_y_momentum
-    else:
-        player_y_momentum += 0.2
-    player_location[1] += player_y_momentum
-    
+    player_movement = [0, player_y_momentum]
     if moving_right:
-        player_location[0] += 4
+        player_movement[0] += 2
     if moving_left:
-        player_location[0] -= 4
+        player_movement[0] -= 2
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+    if player_y_momentum > 3:
+        player_y_momentum = 3
         
-    player_rect.x = player_location[0]
-    player_rect.y = player_location[1]
+    tile_rects = []    
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if game_map[y][x] == '1':
+                display.blit(dirt, (x * TILE_SIZE, y * TILE_SIZE))
+            if game_map[y][x] == '2':
+                display.blit(grass, (x * TILE_SIZE, y * TILE_SIZE))
+            if game_map[y][x] != '0':
+                tile_rects.append(pygame.Rect (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
+         
+    player_rect, collisions = move(player_rect, player_movement, tile_rects)
     
-    if player_rect.colliderect(test_rect):
-        pygame.draw.rect(screen, (255, 0, 0), test_rect)
-    else:
-        pygame.draw.rect(screen, (0, 255, 0), test_rect)
+    if collisions['bottom']:
+        player_y_momentum = 0
+    if collisions['top']:
+        player_y_momentum = 0
+    display.blit(player, (player_rect.x, player_rect.y))
+    
         
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -71,12 +116,14 @@ while True:
                 moving_right = True
             if event.key == K_LEFT:
                 moving_left = True
+            if event.key == K_UP:
+                player_y_momentum = -3
         if event.type == KEYUP:
             if event.key == K_RIGHT:
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
         
-
+    screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
     pygame.display.update()
     clock.tick(60)
